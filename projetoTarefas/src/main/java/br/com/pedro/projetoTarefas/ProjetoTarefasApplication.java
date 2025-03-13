db743922dfe36ab9da62fb98e6291aab0aa96272
psicodev0101 <pedrocsouza97@gmail.com>
2025-03-13 20:24:50 -0300

    initial commit

---
+++ projetoTarefas/src/main/java/br/com/pedro/projetoTarefas/ProjetoTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.projetoTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTarefasApplication.class, args);
+	}
+
+}
